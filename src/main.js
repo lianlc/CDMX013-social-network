0bf4d24370255cb8f89fc9277cbd074218916e88
Celeste Correa <lianlc1710@gmail.com>
2022-09-20 10:37:07 -0500

    Adds a component folder and login content

--- src/main.js
+++ src/main.js
@@ -6,25 +6,21 @@ const root = document.getElementById('root');
 
 const routes = {
   '/': welcome,
-  '/login': login ,
+  '/login': login,
   '/register': register,
-  //'/wall': Wall,
+  '/wall': Wall,
 };
 
-export const onNavigate = (pathname) =>{
-    window.history.pushState(
-        {},
-        pathname,
-        window.location.origin + pathname,
-      );
-      root.removeChild(root.firstChild);
-      root.appendChild(routes[pathname]());
-    };
-    
-    const component = routes[window.location.pathname];
-    window.onpopstate = () => {
-      root.removeChild(root.firstChild);
-      root.append(component());
-    };
-    root.appendChild(component());
-//myFunction();
+export const onNavigate = (pathname) => {
+  window.history.pushState({}, pathname, window.location.origin + pathname);
+  root.removeChild(root.firstChild);
+  root.appendChild(routes[pathname]());
+};
+
+const component = routes[window.location.pathname];
+window.onpopstate = () => {
+  root.removeChild(root.firstChild);
+  root.append(component());
+};
+root.appendChild(component());
+// myFunction();
