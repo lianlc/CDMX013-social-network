1dd343245f51c55fb2bd57889ee61e9a44881c65
Celeste Correa <lianlc1710@gmail.com>
2022-09-27 12:18:59 -0500

    version with the correct flow

--- src/components/login.js
+++ src/components/login.js
@@ -1,6 +1,6 @@
 // import { singInUserWithProviders } from 'https://www.gstatic.com/firebasejs/9.9.4/firebase-auth.js';
 import { onNavigate } from '../main.js';
-import { loginUser, loginwithGoogle, providerGoogle } from '../lib/auth.js';
+import { loginUser } from '../lib/auth.js';
 // import { app } from '../lib/config.js';
 
 export const login = () => {
@@ -8,41 +8,28 @@ export const login = () => {
   const div = document.createElement('div');
   const title = document.createElement('h1');
   const textLogin = document.createElement('p');
-  //const img = document.createElement('img');
+  //  - - - const img = document.createElement('img');
   const divInput = document.createElement('div');
   const inputMail = document.createElement('input');
   const inputPass = document.createElement('input');
   const buttonContinius = document.createElement('button');
-  const google = document.createElement('div');
-  //  - - - const secctionO = document.createElement('p');
-  //  - - - const logoGitHub = document.createElement('img');
-  const logoMail = document.createElement('img');
-  //  - - - const logoTwetter = document.createElement('img');
   const copyright = document.createElement('p');
 
   // CONTENIDO DE LOS ELENETOS **
-  inputMail.setAttribute('id', 'mail');
-  inputPass.setAttribute('id', 'pass');
-  logoMail.setAttribute('id', 'Gmail'); //**
   title.textContent = 'RedEAT';
   textLogin.textContent = 'Inicia sesión';
   divInput.classList = 'divDirection';
   buttonContinius.textContent = 'CONTINUAR';
   buttonContinius.classList = 'buttonStyle';
-  //  - - - secctionO.textContent = '- o -';
   copyright.textContent = 'Todos los derechos reservados, 2022';
-  logoMail.setAttribute('src', 'https://i.postimg.cc/xCQmJ5nw/gmail.png');
-
-  google.addEventListener('click', () => {
-    // singInUserWithProviders(providerGoogle);
-    loginwithGoogle();
-  });
-
+  // ATRIBUTOS  **
+  inputMail.setAttribute('id', 'mail');
+  inputPass.setAttribute('id', 'pass');
   // INSERTA ELEMENTOS AL DIV **
-  google.append(logoMail);
   divInput.append(inputMail, inputPass, buttonContinius);
-  div.append(title, textLogin, divInput, google, copyright);
-
+  div.append(title, textLogin, divInput, copyright);
+  // EVENTOS **
+  // * * INICIO CON CORREO Y CONTRASEÑA * *
   buttonContinius.addEventListener('click', () => {
     const mail = document.getElementById('mail').value;
     const password = document.getElementById('pass').value;
