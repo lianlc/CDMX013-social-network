f9cc76859a254f36814b0351d5fc873409e861b0
AnaDaniela96 <108939830+AnaDaniela96@users.noreply.github.com>
2022-09-29 11:51:58 -0500

    espantosa x

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -2,6 +2,9 @@ import {
   getAuth,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
+  signInWithPopup,
+  //GoogleAuthProvider,
+
 } from 'https://www.gstatic.com/firebasejs/9.9.4/firebase-auth.js';
 import { app } from './config.js';
 
@@ -9,3 +12,5 @@ const auth = getAuth();
 
 export const addUser = (email, password) => createUserWithEmailAndPassword(auth, email, password);
 export const loginUser = (email, password) => signInWithEmailAndPassword(auth, email, password);
+export const loginGoogle = (auth, provider) => signInWithPopup(auth, provider);
+// const provider = new GoogleAuthProvider();
