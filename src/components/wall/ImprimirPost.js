575fe26a7c05fdb29deabd8092dc177ca4324551
AnaDaniela96 <108939830+AnaDaniela96@users.noreply.github.com>
2022-10-18 17:19:58 -0500

    todavía corré el código, excepto el evento: addEventListener del botón borrar.

--- src/components/wall/ImprimirPost.js
+++ src/components/wall/ImprimirPost.js
@@ -1,6 +1,10 @@
-import { getFirestore, collection, onSnapshot, FieldValue } from 'https://www.gstatic.com/firebasejs/9.9.4/firebase-firestore.js';
-// import { getPost } from '../../lib/firestore.js';
-import { app } from '../../lib/config.js';
+/* eslint-disable no-undef */
+/* eslint-disable import/no-unresolved */
+// getFirestore, collection, onSnapshot,
+// import { deletePost, getPost, savePost } from '../../lib/firestore.js';
+import { deletePost, getPost, savePost } from '../../lib/firestore.js';
+// import { app } from '../../lib/config.js';
+import { auth } from '../../lib/auth.js';
 
 export const user = auth.currentUser;
 export const printPost = () => {
@@ -14,9 +18,6 @@ export const printPost = () => {
       console.log(docs.id, '=>', docs.data());
       const post = docs.data();
       const print = document.createElement('div');
-      const titleRestaurant = document.createElement('h3');
-      const contentReview = document.createElement('p');
-      const deletPost = document.createElement('img');
 
       print.classList = 'printPost';
       const contentReview = document.createElement('p');
@@ -31,29 +32,23 @@ export const printPost = () => {
       const editBtn = document.createElement('button');
       editBtn.setAttribute('id', 'editBtn');
       deleteBtn.addEventListener('click', async () => {
-        await deletePost(docs.id);
+        //await deletePost(docs.id);
+        if (savePost.user === user) {
+          deleteBtn.setAttribute('id', 'deleteBtn');
+          print.append(titleRestaurant, contentReview, deleteBtn);
+          deleteBtn.addEventListener('click', async () => {
+            await deletePost(docs.id);
+          });
+        } else {
+          console.log(savePost, user);
+          console.log('no hay user');
+        }
       });
-      // if (savePost.author === user) {
-      //   const deleteBtn = document.createElement('button');
-      //   deleteBtn.setAttribute('id', 'deleteBtn');
-      //   print.append(titleRestaurant, contentReview, deleteBtn);
-      //   deleteBtn.addEventListener('click', async () => {
-      //     await deletePost(docs.id);
-      //   });
-      // } else {
-      //   console.log(savePost.author, user);
-      //   console.log('no hay user');
-      // }
 
       titleRestaurant.setAttribute('id', 'titleRestaurant');
       contentReview.setAttribute('id', 'contentReview');
-      deletPost.setAttribute('src', 'https://i.postimg.cc/x1RTDR3F/delete.png');
-      print.append(titleRestaurant, contentReview, deletPost);
+      print.append(titleRestaurant, contentReview, deleteBtn, editBtn, likeBtn);
       div.append(print);
-
-      deletPost.addEventListener('click', () => {
-        console.log(.delete());
-      });
     });
     // deleteBtn.setAttribute('src', 'https://i.postimg.cc/FHqySXyf/delete-FILL0-wght400-GRAD0-opsz48.png');
   });
--- src/lib/firestore.js
+++ src/lib/firestore.js
@@ -2,8 +2,10 @@
 import {
   getFirestore, addDoc, collection, doc, deleteDoc, onSnapshot,
 } from 'https://www.gstatic.com/firebasejs/9.9.4/firebase-firestore.js';
+import { auth } from './auth.js';
 // import { user } from '../components/wall/ImprimirPost.js';
 import { app } from './config.js';
+
 
 // Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(app);
@@ -12,62 +14,13 @@ const db = getFirestore(app);
 
 export const savePost = (restaurant, review) => {
   const dateCreated = Date.now();
-  // const author = user.uid;
-  // console.log(author);
+  const user = auth.currentUser.uid;
   return addDoc(collection(db, 'post'), {
-    restaurant, review, dateCreated,
+    restaurant, review, dateCreated, user,
   });
 };
 
-// listen when db have changes.
-// export const getPost = () => onSnapshot((collection(db, 'post')));
-
-// export const getPost = () => onSnapshot(colRef, (snapsShot) => {
-//   const reviews = [];
-//   snapsShot.docs.forEach((doc) => {
-//     reviews.push({ ...doc.data() });
-//   });
-//   //console.log(reviews);
-//   return reviews;
-// });
-
-// export const getPost = () => onSnapshot(colRef, (snapsShot) => {
-//   const reviews = [];
-//   snapsShot.docs.forEach((doc) => {
-//     reviews.push({ ...doc.data() });
-//   });
-//   console.log(reviews);
-// });
-
-/** Prueba 1 */
-// import { collection, query, where, onSnapshot } from "firebase/firestore";
-
-// const q = query(collection(db, "cities"), where("state", "==", "CA"));
-// const unsubscribe = onSnapshot(q, (querySnapshot) => {
-//   const cities = [];
-//   querySnapshot.forEach((doc) => {
-//       cities.push(doc.data().name);
-//   });
-//   console.log("Current cities in CA: ", cities.join(", "));
-// });
-
-// export const showDatos = (input) =>
-//  db.collection('post').onSnapshot((querySnapshot) => {
-//     input.innerHTML = '';
-//     querySnapshot.forEach((doc) => {
-//     console.log(`${doc.id} => ${doc.data()}`);
-//     input.innerHTML = + =
-//   });
-// });
-
-// export const getPosts = () => {
-//   db.collection('post').get().then((querySnapshot) => {
-//     querySnapshot.forEach((doc) => {
-//       console.log(`${doc.data().restaurant}`);
-//     });
-//   });
-// };
-
+export const deletePost = async (id) => deleteDoc(doc(db, 'post', id));
 export const getPost = (callback) => {
   onSnapshot(collection(db, 'post'), callback);
 };
