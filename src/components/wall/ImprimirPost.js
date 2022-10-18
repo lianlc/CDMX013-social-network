cb8d01bcd67d4435235f37ca573aa85d454afecb
AnaDaniela96 <108939830+AnaDaniela96@users.noreply.github.com>
2022-10-18 12:02:56 -0500

    x

--- src/components/wall/ImprimirPost.js
+++ src/components/wall/ImprimirPost.js
@@ -1,4 +1,4 @@
-import { getFirestore, collection, onSnapshot } from 'https://www.gstatic.com/firebasejs/9.9.4/firebase-firestore.js';
+import { getFirestore, collection, onSnapshot, FieldValue } from 'https://www.gstatic.com/firebasejs/9.9.4/firebase-firestore.js';
 // import { getPost } from '../../lib/firestore.js';
 import { app } from '../../lib/config.js';
 
@@ -20,24 +20,21 @@ export const printPost = () => {
       const print = document.createElement('div');
       const titleRestaurant = document.createElement('h3');
       const contentReview = document.createElement('p');
+      const deletPost = document.createElement('img');
 
       print.classList = 'printPost';
       titleRestaurant.textContent = post.restaurant;
       titleRestaurant.setAttribute('id', 'titleRestaurant');
       contentReview.textContent = post.review;
       contentReview.setAttribute('id', 'contentReview');
-      print.append(titleRestaurant, contentReview);
+      deletPost.setAttribute('src', 'https://i.postimg.cc/x1RTDR3F/delete.png');
+      print.append(titleRestaurant, contentReview, deletPost);
       div.append(print);
+
+      deletPost.addEventListener('click', () => {
+        console.log(.delete());
+      });
     });
   });
-
   return div;
 };
-
-getPost(() => {
-  onSnapshot((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-    console.log(`${doc.data().restaurant}`);
-    });
-  });
-});
